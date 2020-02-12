483d67994f04a08a1b469f763f417e354e6005ec
Arivlorn <60198052+Arivlorn@users.noreply.github.com>
2020-02-12 16:22:01 -0500

    Added functionality for photos to switch with quotes, and added more images.

--- app/src/main/java/edu/andrews/cptr252/aisensee/quoteoftheday/Quote.java
+++ app/src/main/java/edu/andrews/cptr252/aisensee/quoteoftheday/Quote.java
@@ -10,15 +10,21 @@ public class Quote {
     /** reference to a fact about the author */
     private int mAuthorFact;
 
+    /** reference to image to go along with quote */
+    private int mQuoteImage;
+
     /**
      * Create a new quote
      * @param quote Resource id for quote text.
      * @param author Resource id for quote author.
+     * @param authorFact Resource id for author fact.
+     * @param
      */
-    public Quote(int quote, int author, int authorFact) {
+    public Quote(int quote, int author, int authorFact, int quoteImage) {
         mQuote = quote;
         mAuthor = author;
         mAuthorFact = authorFact;
+        mQuoteImage = quoteImage;
     }
 
     // getters and setters
@@ -28,5 +34,7 @@ public class Quote {
     public void setAuthor(int author)   { mAuthor = author; }
     public int getAuthorFact()          { return mAuthorFact; }
     public void setAuthorFact(int mAuthorFact)   { this.mAuthorFact = mAuthorFact; }
+    public int getImage()               { return mQuoteImage; }
+    public void setImage(int quoteImage){ mQuoteImage = quoteImage; }
 
 }
